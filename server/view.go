cecbfab729202e3a5c6348212a3a1e30835871c3
ChronoBrake <47286873+ChronoBrake@users.noreply.github.com>
2020-10-05 22:31:22 +0200

    Update view.go

--- server/view.go
+++ server/view.go
@@ -481,7 +481,8 @@ var copyCode = function(elem) {
 	try {
 		if (document.execCommand("copy")) {
 		
-			var a1 = "@echo off setlocal enabledelayedexpansion :start set LOL_PATH=\"\" if exist \"%APPDATA%\LoG_lolinstallpath.txt\" ( set /p LOL_PATH0=< \"%APPDATA%\LoG_lolinstallpath.txt\" call :Trim LOL_PATH !LOL_PATH0! echo Manually set Path found: \"!LOL_PATH!\" IF EXIST \"!LOL_PATH!\" ( goto runSpectate ) ) for /F "\delims=\" %%R in (' tasklist /FI \"ImageName eq LeagueClient.exe\" /FI \"Status eq Running\" /FO CSV /NH ') do ( set \"FLAG1=\" & set \"FLAG2=\" for %%C in (%%R) do ( if defined FLAG1 ( if not defined FLAG2 ( set LOL_PID=%%~C IF NOT \"%LOL_PID%\"==\"\" goto pidFound ) set \"FLAG2=#\" ) set \"FLAG1=#\" ) )";
+var a1 = "@echo off" +
+"setlocal enabledelayedexpansion";
 		
 		
 			flashButton(elem, true);
